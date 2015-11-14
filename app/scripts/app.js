334121ceacd146eda0cc0ba76093a6986bb15be9
brapastor <brapastor@gmail.com>
2015-11-13 23:44:57 -0500

    Agregado de services factorys

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,33 @@
-(function(){
+(function() {
   'use strict';
   // Aqui ira la funcionalidad
-  angular.module('blog',['ngRoute','blog.controllers'])
-         .config(config);
+  angular.module('blog', ['ngRoute', 'blog.controllers']);
 
   function config($locationProvider, $routeProvider) {
+    // es importante, ya que permite que las URLs no lleven el caracter # al
+    // inicio de ellas, como utiliza por defecto Angular.
     $locationProvider.html5Mode(true);
 
     $routeProvider
       .when('/', {
-         templateUrl: 'views/post-list.tpl.html',
-         controller: 'PostListCtrl',
-         controllerAs: 'postlist'
+        templateUrl: 'views/post-list.tpl.html',
+        controller: 'PostListCtrl',
+        controllerAs: 'postlist'
       })
-       .when('/post/:postId', {
-       templateUrl: 'views/post-detail.tpl.html',
-       controller: 'PostDetailCtrl',
-       controllerAs: 'postdetail'
-       })
+      .when('/post/:postId', {
+        templateUrl: 'views/post-detail.tpl.html',
+        controller: 'PostDetailCtrl',
+        controllerAs: 'postdetail'
+      })
 
-       .when('/new', {
-       templateUrl: 'views/post-create.tpl.html',
-       controller: 'PostCreateCtrl',
-       controllerAs: 'postcreate'
-       });
+    .when('/new', {
+      templateUrl: 'views/post-create.tpl.html',
+      controller: 'PostCreateCtrl',
+      controllerAs: 'postcreate'
+    });
   }
 
+  angular
+    .module('blog')
+    .config(config);
 })();
