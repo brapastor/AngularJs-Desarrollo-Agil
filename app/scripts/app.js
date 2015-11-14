3ed0b9029d88b52bff83d3f34a22868175a7606b
brapastor <brapastor@gmail.com>
2015-11-13 22:34:50 -0500

    primer commit

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+(function(){
+  'use strict';
+  // Aqui ira la funcionalidad
+  angular.module('blog',['ngRoute','blog.controllers'])
+         .config(config);
+
+  function config($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+         templateUrl: 'views/post-list.tpl.html',
+         controller: 'PostListCtrl',
+         controllerAs: 'postlist'
+      })
+       .when('/post/:postId', {
+       templateUrl: 'views/post-detail.tpl.html',
+       controller: 'PostDetailCtrl',
+       controllerAs: 'postdetail'
+       })
+
+       .when('/new', {
+       templateUrl: 'views/post-create.tpl.html',
+       controller: 'PostCreateCtrl',
+       controllerAs: 'postcreate'
+       });
+  }
+
+})();
